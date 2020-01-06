f80e2443e918a3564ca7919f15efc00a3be387f6
Keith <keithtmccartney@hotmail.com>
2020-01-06 21:45:39 +0000

    Updating the 'Startup' to allow Cross-Origin Resource Sharing within the configuration.

--- bug-free-bassoon/Blog/Startup.cs
+++ bug-free-bassoon/Blog/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore;
 using Blog.Models;
+using Blog.Data;
 
 namespace Blog
 {
@@ -31,6 +32,22 @@ namespace Blog
 
             services.AddDbContext<BlogPostsContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("BlogPostsContext")));
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+            });
+
+            services.AddScoped(typeof(IDataRepository<>), typeof(DataRepository<>));
+
+            // In production, the Angular files will be served from this directory.
+            //services.AddSpaStaticFiles(configuration =>
+            //{
+            //    configuration.RootPath = "ClientApp/dist";
+            //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
