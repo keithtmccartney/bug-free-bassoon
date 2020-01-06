153bd8221a51d6f63c219c6d1644593de6fc5b1e
Keith <keithtmccartney@hotmail.com>
2020-01-06 21:26:33 +0000

    Creating a 'BlogPost' Model with 'BlogPostsController' and 'BlogPostsContext' Data Context.

--- bug-free-bassoon/Blog/Startup.cs
+++ bug-free-bassoon/Blog/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using Blog.Models;
 
 namespace Blog
 {
@@ -26,6 +28,9 @@ namespace Blog
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<BlogPostsContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("BlogPostsContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
